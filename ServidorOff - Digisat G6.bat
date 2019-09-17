02f1226e04a750f905aa4881192eae291c2c3146
RonaldsS <33934560+Ronaldss@users.noreply.github.com>
2019-09-17 17:14:42 -0300

    Add files via upload
    Verificar diretório padrão dos arquivos de configuração.

---
+++ ServidorOff - Digisat G6.bat
@@ -0,0 +1,36 @@
+@echo off
+@echo.
+color 9f
+cls
+@echo Finalizando aplicativos DigiSat
+taskkill /f /im ServidorG6.exe
+taskkill /f /im SistemaG6.exe
+@echo > nao encontrado = sistema finzalizado ja encontrava-se finalizado.
+@echo.
+timeout 3 >nul
+cls
+@echo.
+@echo Reinicia o MongoDB e Sincronizador
+net stop MongoDBDigisat
+net stop SincronizadorDigisat
+timeout 2 >nul
+rem C:\DigiSat\SuiteG6\Servidor\nomeArquivo outroNome
+rem C:\DigiSat\SuiteG6\Sistema\nomeArquivo outroNome
+timeout 1 >nul
+net start MongoDBDigisat
+net start SincronizadorDigisat
+timeout 3 >nul
+cd\
+cd DigiSat\SuiteG6\Servidor
+start ServidorG6.exe
+timeout 3 >nul
+cls
+@echo.
+@echo    ***********************************************
+@echo    ***                                         ***
+@echo    ***      Processo Finalizado com sucesso    ***
+@echo    ***      Por RonaldsS                       ***
+@echo    ***                                         ***
+@echo    ***********************************************
+@echo.
+cls
